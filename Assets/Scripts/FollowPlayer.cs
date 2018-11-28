63630ab16dd85ca5b948d83826e224f20bc6dbef
Salvaje96 <33075020+Salvaje96@users.noreply.github.com>
2018-11-27 18:50:18 -0700

    Enviro test + camera follow
    Updated Enviro test, added a camera follow function, and downloaded terrain tools.

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public float CameraMoveSpeed = 120.0f;
+    public GameObject cameraFollowObj;
+    Vector3 followPOS;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CameraUpdater();
+	}
+
+    void CameraUpdater()
+    {
+        Transform target = cameraFollowObj.transform;
+
+        float step = CameraMoveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
