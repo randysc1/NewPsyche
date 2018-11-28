d1ff5264dec0acb48ea4bb3f29b2c5857a692493
Salvaje96 <33075020+Salvaje96@users.noreply.github.com>
2018-11-27 21:40:50 -0700

    Dual Camera follow

---
+++ Assets/Scripts/FallowDualPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallowDualPlayer : MonoBehaviour
+{
+
+    public float CameraMoveSpeed = 120.0f;
+    public GameObject cameraFollowObj1;
+    public GameObject cameraFollowObj2;
+    Vector3 followPOS;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CameraUpdater();
+    }
+
+    void CameraUpdater()
+    {
+        Transform target1 = cameraFollowObj1.transform;
+        Transform target2 = cameraFollowObj2.transform;
+
+        print(target1.position.x);
+
+        float step = CameraMoveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, (target1.position + target2.position)/2, step);
+    }
+}
